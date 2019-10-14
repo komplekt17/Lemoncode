fa2884c4be1bba0377a306a87521ddd4377a2451
komplekt17 <komplekt_17@mail.ru>
2019-10-14 16:53:50 +0500

    add react

---
+++ react-typescript-samples/old_class_components/src/components/HelloPage.tsx
@@ -0,0 +1,7 @@
+import * as React from "react"
+
+const HelloComponent = () => {
+	return <h2>Helo React!</h2>
+}
+
+export { HelloComponent }
---
+++ react-typescript-samples/old_class_components/src/container/App.tsx
@@ -0,0 +1,8 @@
+import * as React from "react"
+import { HelloComponent } from "../components"
+
+const App = () => {
+	return <HelloComponent />
+}
+
+export default App
